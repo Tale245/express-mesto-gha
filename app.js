4c7ad0ba2eb9411b0162b9d44fef6cb93475e367
Renat Agaev <reker.207@mail.ru>
2022-11-09 16:57:32 +0300

    Первый коммит проектной работы, сделана основная работа

---
+++ app.js
@@ -0,0 +1,198 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const user = require("./models/user");
+const card = require("./models/card");
+const bodyParser = require("body-parser");
+const app = express();
+
+mongoose.connect("mongodb://localhost:27017/mestodb");
+
+app.use(bodyParser.json());
+
+// РОУТЫ ДАННЫХ ПОЛЬЗОВАТЕЛЯ
+
+// получение данных пользователя
+app.get("/users", (req, res) => {
+  user
+    .find({})
+    .then((data) => res.status(200).send(data))
+    .catch((e) => {
+      if (e.name === "ValidationError") {
+        return res
+          .status(404)
+          .send({ message: "Запрашиваемый пользователь не найден" });
+      } else {
+        return res.status(500).send({ message: "Произошла ошибка" });
+      }
+    });
+});
+
+// получение данных конкретного пользователя по id
+app.get("/users/:id", (req, res) => {
+  user
+    .findById(req.params.id)
+    .then((data) => res.status(200).send(data))
+    .catch((e) => res.status(500).send(e));
+});
+
+// создание пользователя
+app.post("/users", (req, res) => {
+  const { name, about, avatar } = req.body;
+  user
+    .create({ name, about, avatar })
+    .then((data) => res.status(200).send(data))
+    .catch((e) => {
+      if (e.name === "ValidationError") {
+        return res
+          .status(400)
+          .send({ message: "Переданы некорректные данные" });
+      } else {
+        return res.status(500).send({ message: "Произошла ошибка" });
+      }
+    });
+});
+
+// хардкод айди пользователя, создавшего краточку
+app.use((req, res, next) => {
+  req.user = {
+    _id: "636974c5ff0cbf5637c13f3f", // вставьте сюда _id созданного в предыдущем пункте пользователя
+  };
+  next();
+});
+
+// РОУТУ ДАННЫХ КАРТЧКИ
+
+// получение данных всех карточек
+app.get("/cards", (req, res) => {
+  card
+    .find({})
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((e) => {
+      if (e.name === "ValidationError") {
+        return res
+          .status(404)
+          .send({ message: "Запрашиваемая карточка не найдена" });
+      } else {
+        return res.status(500).send({ message: "Произошла ошибка" });
+      }
+    });
+});
+
+// создание карточки
+app.post("/cards", (req, res) => {
+  const { name, link, owner = req.user._id } = req.body;
+
+  card
+    .create({ name, link, owner })
+    .then((data) => res.status(200).send(data))
+    .catch((e) => {
+      if (e.name === "ValidationError") {
+        return res
+          .status(400)
+          .send({ message: "Переданы некорректные данные" });
+      } else {
+        return res.status(500).send({ message: "Произошла ошибка" });
+      }
+    });
+});
+// удаление карточки по id
+app.delete("/cards/:id", (req, res) => {
+  card
+    .findByIdAndRemove(req.params.id)
+    .then((data) => res.status(200).send(data))
+    .catch((e) => {
+      if (e.name === "ValidationError") {
+        return res
+          .status(400)
+          .send({ message: "Переданы некорректные данные" });
+      } else {
+        return res.status(500).send({ message: "Произошла ошибка" });
+      }
+    });
+});
+
+// ДОПОЛНИТЕЛЬНЫЕ РОУТЫ
+
+// обновление профиля
+app.patch("/users/me", (req, res) => {
+  const { name, about } = req.body;
+
+  user
+    .findByIdAndUpdate(req.user._id, { name: name, about: about })
+    .then((data) => res.status(200).send(data))
+    .catch((e) => {
+      if (e.name === "ValidationError") {
+        return res
+          .status(400)
+          .send({ message: "Переданы некорректные данные" });
+      } else {
+        return res.status(500).send({ message: "Произошла ошибка" });
+      }
+    });
+});
+
+// обновление аватара профиля
+app.patch("/users/me/avatar", (req, res) => {
+  const { avatar } = req.body;
+
+  user
+    .findByIdAndUpdate(req.user._id, { avatar: avatar })
+    .then((data) => res.status(200).send(data))
+    .catch((e) => {
+      if (e.name === "ValidationError") {
+        return res
+          .status(400)
+          .send({ message: "Переданы некорректные данные" });
+      } else {
+        return res.status(500).send({ message: "Произошла ошибка" });
+      }
+    });
+});
+
+// поставить лайк карточке
+app.put("/cards/:cardId/likes", (req, res) => {
+  card
+    .findByIdAndUpdate(
+      req.params.cardId,
+      { $addToSet: { likes: req.user._id } },
+      { new: true }
+    )
+    .then((data) => res.status(200).send(data))
+    .catch((e) => {
+      if (e.name === "ValidationError") {
+        return res
+          .status(400)
+          .send({ message: "Переданы некорректные данные" });
+      } else {
+        return res.status(500).send({ message: "Произошла ошибка" });
+      }
+    });
+});
+
+// удалить лайк у карточки
+app.delete("/cards/:cardId/likes", (req, res) => {
+  card
+    .findByIdAndUpdate(
+      req.params.cardId,
+      { $pull: { likes: req.user._id } },
+      { new: true }
+    )
+    .then((data) => res.status(200).send(data))
+    .catch((e) => {
+      if (e.name === "ValidationError") {
+        return res
+          .status(400)
+          .send({ message: "Переданы некорректные данные" });
+      } else {
+        return res.status(500).send({ message: "Произошла ошибка" });
+      }
+    });
+});
+
+const { PORT = 3000 } = process.env;
+
+app.listen(PORT, () => {
+  console.log(`server has been started on port ${PORT}`);
+});
---
+++ models/card.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const objectId = mongoose.Schema.ObjectId
+
+const cardSchema = new mongoose.Schema({
+  name:{
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: objectId,
+    required: true
+
+  },
+  likes: [{
+    type: objectId,
+    default: []
+  }],
+  createsAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('card', cardSchema)
