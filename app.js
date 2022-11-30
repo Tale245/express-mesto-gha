5093416645acee199f4c74e093e647d3eb279eaf
Renat Agaev <reker.207@mail.ru>
2022-11-30 23:17:04 +0300

    Проектная работа готова 6

--- app.js
+++ app.js
@@ -19,7 +19,13 @@ app.use(bodyParser.json());
 
 // хардкод айди пользователя, создавшего краточку
 
-app.post('/signin', login);
+// app.post('/signin', login);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), login);
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
