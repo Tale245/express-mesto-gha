896cc0f920ced3ae0f57cd0e832dd0e68ab9fbea
Renat Agaev <reker.207@mail.ru>
2022-11-30 23:33:33 +0300

    Проектная работа готова 9

--- routes/card.js
+++ routes/card.js
@@ -22,13 +22,13 @@ router.delete('/cards/:id', celebrate({
   }),
 }), deleteCard);
 router.put('/cards/:cardId/likes', celebrate({
-  body: Joi.object().keys({
+  params: Joi.object().keys({
     cardId: Joi.string().required().alphanum().length(24),
   }),
 }), likeCard);
 router.delete('/cards/:cardId/likes', celebrate({
-  body: Joi.object().keys({
-    cardId: Joi.string().alphanum().length(24),
+  params: Joi.object().keys({
+    cardId: Joi.string().required().alphanum().length(24),
   }),
 }), dislikeCard);
 
